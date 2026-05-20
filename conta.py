631f2c48dccd044a19ef92b7907bea37f101bbc6
master-one-prog <pietromacmiranda@gmail.com>
2026-05-20 16:20:10 -0300

    essa e a classe

---
+++ conta.py
@@ -0,0 +1,18 @@
+class Conta():
+    def __init__(self, saldo):
+        self.saldo = saldo
+    def sacar(self, retirada):
+        saldo = saldo - retirada 
+        print("Saque realizado com sucesso!!")
+        print(f"Valor do saque: {retirada}")
+        print(f"Saldo: {saldo}")
+
+    def depositar(self, deposito):
+        saldo += deposito
+        print("Depósito realizado com sucesso!!")
+        print(f"Valor do depósito: {deposito}")
+        print(f"Saldo: {saldo}")
+
+    def render(self):
+        saldo *= 0.1
+        return saldos
